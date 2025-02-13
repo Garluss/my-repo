f4317d2145dc426a6db45c9dd31c889b74aae776
Daniel Olsen <daniel.boenes@gmail.com>
2025-02-13 13:22:20 +0100

    Fikk plotting til å virke
    Passe stor oppdatering

--- prosj/VaerdataHawaii/behandling2.py
+++ prosj/VaerdataHawaii/behandling2.py
@@ -13,24 +13,18 @@ with open(filnavn,mode="r") as fil:
     for i in innhold:
         behandlet[i[0]] = []
         for y in range(1,4):
-            behandlet[i[0]] = []
-            for y in range(1,4):
-                if i[y] != "":
-                    behandlet[i[0]].append(int(i[y]))
-                else:
-                    behandlet[i[0]].append(int(previous[y]))
-            previous.clear()
-            previous.append(i[0])
-            for y in range(0,3):
-                previous.append(behandlet[i[0]][y])
-
-    
+            try:
+                behandlet[i[0]].append(int(i[y]))
+            except:
+                behandlet[i[0]].append(None)
 xpoints = []
 ypoints = []
-colors = []
 t = 0
 for i in behandlet:
-    value = (behandlet[i][1]+behandlet[i][2])/2
+    if behandlet[i][1] != None and behandlet[i][2] != None:
+        value = (behandlet[i][1]+behandlet[i][2])/2
+    else:
+        value = None
     xpoints.append(t)
     ypoints.append(value)
     t += 1
@@ -38,17 +32,19 @@ for i in behandlet:
 Plot, Axis = plt.subplots()
 plt.subplots_adjust(bottom=0.25)
 
-plt.scatter(xpoints,ypoints,c=ypoints,cmap="viridis",s=2)
+plt.scatter(xpoints,ypoints,c=ypoints,cmap="viridis",s=3)
 plt.title("Temperatur i Hawaii (Honolulu Lufthavn)")
 plt.xlabel("Antall dager fra 1940 14. mai (1940 - 2025)")
 plt.ylabel("Temperatur målt i fahrenheit")
+plt.xlim(0,500)
+plt.ylim(60,90)
 plt.colorbar()
 
 slider_color = 'White'
  
 # Set the axis and slider position in the plot
 axis_position = plt.axes([0.2, 0.1, 0.65, 0.03],facecolor = slider_color)
-slider_position = Slider(axis_position, 'Pos', 0.1, 29500.0)
+slider_position = Slider(axis_position, 'Pos', 0.1, 30500.0)
  
 def update(val):
     pos = slider_position.val
